86240e929fa3061abd88d9ae649057ecf665953f
WanjikuSharon <sharonwanjiku292@gmail.com>
2024-11-12 13:14:49 +0300

    Assignment

---
+++ assignment1.py
@@ -0,0 +1,10 @@
+# create an atm machine that awards discounts to users depending on their
+
+# withdrawal amount and display the total amount inclusive of the discounts.
+
+# Withdrawal above 10,000 award a discount of 15%, withdrawal above 5000 give
+
+# a discount of 10% and lastly a withdrawal of above 2000 give a discount of
+
+# 5%
+
