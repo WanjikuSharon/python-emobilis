b21f35f4536d0f5fbe0ae83cd3888ade07494e59
WanjikuSharon <sharonwanjiku292@gmail.com>
2024-11-12 12:17:24 +0300

    Hey

--- variablesinput.py
+++ variablesinput.py
@@ -1,1 +1,13 @@
 print("Sharon")
+name = input("Whats your name: ")
+print(name)
+
+height = input("Whats your height: ")
+print("Your height is " +height + " inch")
+
+#Type conversion
+Fav_number =input("Your Favourite number? ")
+Least_number = input("Least Number? ")
+
+sum = int(Fav_number) + int(Least_number)
+print(sum)
