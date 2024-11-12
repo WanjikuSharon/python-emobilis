2b315e40ce83be19246bee40e281e4ac5971ebb8
WanjikuSharon <sharonwanjiku292@gmail.com>
2024-11-12 18:13:40 +0300

    Operators

--- operators.py
+++ operators.py
@@ -14,7 +14,17 @@ print(x)
 z = 4
 s = 11
 print(s==7)
-#Logical operators
+#Logical operators/Class Asignment
+sharon = 22
+phillip = 24
+allan = 13
+ivy = 21
+
+print(sharon >= 22 and phillip <= 24)
+print(sharon > 21 and phillip < 24)
+print(allan > 12 and ivy < 24)
+print(allan > 23 and ivy < 24)
+
 #Identity operators
 #Membership operators
 #Bitwise operators
