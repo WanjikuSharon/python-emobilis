1ff855fb1ef543ccf75c63728a0a8f73b79f186d
WanjikuSharon <sharonwanjiku292@gmail.com>
2024-11-12 15:11:07 +0300

    ATM withdrawal with discount

--- assignment1.py
+++ assignment1.py
@@ -1,4 +1,4 @@
-# create an atm machine that awards discounts to users depending on their
+# create an atm that awards discounts to users depending on their
 
 # withdrawal amount and display the total amount inclusive of the discounts.
 
@@ -8,3 +8,25 @@
 
 # 5%
 
+Name = input("What's your name? ")
+print("Hello " + Name)
+
+# Get the withdrawal amount
+Withdrawal_Amount = int(input("Enter Amount to Withdraw: "))
+print("Withdrawing... " + str(Withdrawal_Amount))
+
+# Initialize discount to 0 in case no discount applies
+Discount = 0
+
+if Withdrawal_Amount > 10000:
+    Discount = 0.15 * Withdrawal_Amount
+elif Withdrawal_Amount > 5000 and Withdrawal_Amount <= 10000:
+    Discount = 0.1 * Withdrawal_Amount
+elif Withdrawal_Amount > 2000 and Withdrawal_Amount <= 5000:
+    Discount = 0.05 * Withdrawal_Amount
+else:
+    print("You have no discount on a withdrawal of " + str(Withdrawal_Amount))
+
+# Calculate the total withdrawal after discount
+Total_Withdrawal = Withdrawal_Amount - Discount
+print("Total amount after discount: " + str(Total_Withdrawal))
