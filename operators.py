b714af041b36740ce1d0074b0b5f747d0f723cd0
WanjikuSharon <sharonwanjiku292@gmail.com>
2024-11-12 18:06:30 +0300

    Comparison Operators

---
+++ operators.py
@@ -0,0 +1,20 @@
+#Arithmetic operators
+a=2
+b=9
+print(a+b)
+print(a-b)
+print(a*b)
+print(a%b)
+print(a/b)
+#Assignment operators
+x = 5
+x += 3
+print(x)
+#Comparison operators
+z = 4
+s = 11
+print(s==7)
+#Logical operators
+#Identity operators
+#Membership operators
+#Bitwise operators
