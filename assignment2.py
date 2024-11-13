c1fd59e585d79496853d879fd8386400b9f0678d
WanjikuSharon <sharonwanjiku292@gmail.com>
2024-11-13 12:05:07 +0300

    Asignment ii

---
+++ assignment2.py
@@ -0,0 +1,7 @@
+# create a function called greet that takes a persons name as an argument and returns
+# a greeting message. if the name is 'Alice' or 'Bob' it returns a personalised
+# greeting, for any other name it should return a general greeting
+
+# write a python program that finds a maximum of three numbers
+
+# create a python function that sums all numbers in a list
