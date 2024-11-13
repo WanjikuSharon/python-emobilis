b6a59bec6b1845c120504b87d5743c31fb70709d
WanjikuSharon <sharonwanjiku292@gmail.com>
2024-11-13 11:16:09 +0300

    string interpolation using the f-strings

--- functions.py
+++ functions.py
@@ -17,3 +17,13 @@ def my_function(*kids):
   print("The youngest child is " + kids[2])
 
 my_function("Emil", "Tobias", "Linus")
+
+#Ass: Function that takes 2 numbers as arguments and performs the summation and display the summation
+
+def my_summation(one, two):
+    # Ensure the arguments are treated as numbers
+    result = int(one) + int(two)
+    print(f"The sum is {result}")
+
+# Call the function with valid arguments
+my_summation(8, 9)
