fd4383e1215af2443173e2ae9847f823e524995c
WanjikuSharon <sharonwanjiku292@gmail.com>
2024-11-13 10:33:26 +0300

    Calling functions and passing arguments

---
+++ functions.py
@@ -0,0 +1,13 @@
+def my_function():
+    print("Hello Sharon")
+
+#calling
+my_function()
+
+def my_family(name):
+    print(name + " Simwa")
+
+#calling
+my_family("Sharon")
+my_family("Allan")
+my_family("Jasmine")
