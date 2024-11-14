97b3c33bc870580fd52df9e9ad45f4e9ce61d147
WanjikuSharon <sharonwanjiku292@gmail.com>
2024-11-14 15:07:53 +0300

    Classes

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ class Student:
     last_name = "Wanjiku"
     gender = "Female"
     age = "22"
+
+
+class Person:
+    def __init__(self, name,gender,marital_status,occupation):
+        self.name = name
+        self.gender = gender
+        self.marital_status = marital_status
+        self.occupation = occupation
--- object.py
+++ object.py
@@ -2,3 +2,6 @@ from main import Student
 
 student1 = Student()
 print(student1.first_name +" " + student1.last_name)
+
+student2 = Student()
+print(student2.first_name)
