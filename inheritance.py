53dd5c694f4559c0dba09f0c5aad6a969a9fed8d
WanjikuSharon <sharonwanjiku292@gmail.com>
2024-11-14 17:31:42 +0300

    Implementation of the child and parent class objects

---
+++ inheritance.py
@@ -0,0 +1,10 @@
+from main import Employee, Manager
+
+#parent class object implementation
+accountant = Employee("Sharon Wanjiku", 22, "Female", "30,000")
+security = Employee("Erastus Mwangi", 34, "Male", 50000 )
+
+#child object
+sot = Manager("Jasmine Wanjiku", 20, "Female", 40000, "Masters")
+
+
